426fe23ac52cb7c9f372a87a20cd17f69e2f17c9
Greg Lutostanski <gregory.lutostanski@canonical.com>
2015-11-12 22:25:24 -0600

    first version of lxc-cmd

---
+++ lxc-cmd.sh
@@ -0,0 +1,40 @@
+function lxc-ip {
+    lxc-ls --fancy | grep -E "^$1[[:space:]]" | awk '{print $3}'
+}
+
+function lxc-waiting {
+    while [ $(lxc-ip $1) == '-' ]; do sleep 0.1; done; sleep 0.5
+}
+
+function lxc-ssh {
+    grep -qF '#lxc' ~/.ssh/config || echo '#lxc' >> ~/.ssh/config
+    sed '/^#lxc/q' < ~/.ssh/config > ~/.ssh/config_temp
+    cat ~/.ssh/config_temp > ~/.ssh/config
+    rm ~/.ssh/config_temp
+    lxc-ls --fancy | awk 'NR>2 && $3 != "-" {if(index($3,",") > 0) {ip=substr($3,1,index($3,",")-1) } else { ip=$3 }; print "Host "$1"\nForwardAgent yes\nStrictHostKeyChecking no\nUserKnownHostsFile=/dev/null\nUser ubuntu\nHostName " ip}' >> ~/.ssh/config
+}
+
+function lxc {
+    if [ -z "$1" ]
+    then
+        lxc-ls --fancy | awk '$3 != "-" {print}'
+    else
+        CREATE=0
+        lxc-ls -1 | grep -qF $1 || lxc-clone -s "${2:-"clean"}" $1
+        if [ $(lxc-ip $1) == '-' ]
+        then 
+            lxc-start -dn $1; lxc-waiting $1;
+        fi
+        lxc-ssh
+        ssh $1 2>/dev/null
+    fi
+}
+
+function lxc-autocomplete {
+    reply=( $(lxc-ls -1) )
+}
+
+
+compctl -K lxc-autocomplete lxc
+compctl -K work-autocomplete work
+
