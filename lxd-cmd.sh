d5d13f225829741bf4be91be5a608b1269be3f33
Greg Lutostanski <gregory.lutostanski@canonical.com>
2016-09-26 14:45:45 -0500

    change for lxd

--- lxd-cmd.sh
+++ lxd-cmd.sh
@@ -1,23 +1,23 @@
-function lxc-ip {
-    lxc-ls --fancy | grep -E "^$1[[:space:]]" | awk '{print $3}'
+function lxd-ip {
+    lxc-ls --fancy | grep -E "^$1[[:space:]]" | awk '{print $5}'
 }
 
-function lxc-waiting {
+function lxd-waiting {
     while [ $(lxc-ip $1) == '-' ]; do sleep 0.1; done;
 }
 
-function lxc-ssh {
+function lxd-ssh {
     grep -qF '#lxc' ~/.ssh/config || echo '#lxc' >> ~/.ssh/config
     sed '/^#lxc/q' < ~/.ssh/config > ~/.ssh/config_temp
     cat ~/.ssh/config_temp > ~/.ssh/config
     rm ~/.ssh/config_temp
-    lxc-ls --fancy | awk 'NR>2 && $3 != "-" {if(index($3,",") > 0) {ip=substr($3,1,index($3,",")-1) } else { ip=$3 }; print "Host "$1"\nForwardAgent yes\nUser ubuntu\nHostName " ip "\nHostKeyAlias "$1}' >> ~/.ssh/config
+    lxc list -c n4 | grep -Ev '^\+-' | tail -n +2 | tr -d ' ' | awk -F '|' '$3 != "" {ip=substr($3,1,index($3,"(")-1); print "Host "$2"\nForwardAgent yes\nUser ubuntu\nHostName " ip "\nHostKeyAlias "$2}' >> ~/.ssh/config
 }
 
-function lxc {
+function lxe {
     if [ -z "$1" ]
     then
-        lxc-ls --fancy | awk '$3 != "-" {print}'
+        lxc list --running -f
     else
         CREATE=0
         lxc-ls -1 | grep -qF $1 || lxc-clone -s "${2:-"clean"}" $1
@@ -29,9 +29,9 @@ function lxc {
     fi
 }
 
-function lxc-autocomplete {
+function lxd-autocomplete {
     reply=( $(lxc-ls -1) )
 }
 
 
-compctl -K lxc-autocomplete lxc
+compctl -K lxd-autocomplete lxe
